265ce0502478f105dfb4c65eb88824ea1a2fc0f4
Nina24-03 <nimi.stadler@googlemail.com>
2025-05-03 09:19:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = 'dein_geheimer_schluessel'  # Stelle sicher, dass dies sicher ist!
